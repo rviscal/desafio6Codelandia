e78929bd6ec282fc32aab598aa780cb6bd96034d
rviscal <113106260+rviscal@users.noreply.github.com>
2023-03-26 19:57:35 -0300

    first commit

---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Background } from './Style';
+
+
+function Body() {
+  return (
+    <Background>
+      <div> teste</div>
+    </Background>
+       
+   
+  
+    
+    
+  )
+}
+
+export default Body;
---
+++ src/Components/Body/Style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import image from "../../Img/background.jpg"
+
+export const Background = styled.body`
+    display:flex;
+    flex-direction:column;
+    align-items: center;
+    background-image:url(${image});
+    background-position: center top;
+    background-size:100% 100%;
+    
+ 
+`;
