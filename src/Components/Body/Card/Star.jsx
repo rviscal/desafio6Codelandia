4bd5516892daeb3e2b13842be2db224227e10bca
rviscal <113106260+rviscal@users.noreply.github.com>
2023-03-27 19:08:57 -0300

    stars responsive

--- src/Components/Body/Card/Star.jsx
+++ src/Components/Body/Card/Star.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import starfill from "../../../Img/star.png";
-import starOut from "../../../Img/starout.png";
+import starOut from "../../Assets/Img/starout.png";
 import { StarFill } from "./Style";
 
-const Starfill = [1, 2, 3, 4];
+const Starfill = [1, 2, 3, 4, 5];
 
-function Star() {
+function Star(props) {
+  let stars = {
+    starfill: starfill,
+    starout: starOut,
+  };
+  const [st, setSt] = useState("starout");
+  const SwapStar = () => {
+    setSt((st) => (st === "starout" ? "starfill" : "starout"));
+  };
   return (
     <>
       {Starfill.map((item) => (
-        <StarFill src={starfill} alt="Star" />
+        <StarFill src={stars[st]} alt="Star" onClick={SwapStar} />
       ))}
-      <StarFill src={starOut} alt="StarOut" />
     </>
   );
 }
